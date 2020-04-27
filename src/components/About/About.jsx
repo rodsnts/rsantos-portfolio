364c7b08afde55fd8c3963b83e009fa3701811f2
Rodrigo Santos <rr.santos1232@gmail.com>
2020-04-27 21:25:07 +0100

    json api

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 
+const db = require("../../db.json");
+
 const About = () => {
   return (
     <section className="about">
-      <p>RODRIGO SANTOS</p>
+      <p
+        style={{ textTransform: "uppercase" }}
+      >{`${db.profile.firstName} ${db.profile.lastName}`}</p>
+
       <h1>ABOUT ME</h1>
-      <p>
-        Hi my name is Rodrigo.{" "}
-        <span className="mark">
-          I'm a UI/UX designer I've beem designing for about 3 years now.
-        </span>{" "}
-        I love working with creative individuals that can bring strong critique
-        as well as laugh at latest meme.
-      </p>
-      <a href="rsantos.dev">SUBSCRIBE TO OUR NEWSLETTER</a>
+      <p>{db.about.me}</p>
+
+      <span
+        className="mark"
+        style={{ width: "fit-content", marginTop: "20px" }}
+      >
+        <a href="rsantos.dev">SUBSCRIBE TO OUR NEWSLETTER</a>
+      </span>
 
       <p style={{ marginTop: "45px" }}>EXPERIENCE :</p>
 
-      <p>2016 to present PayPay inc.</p>
+      <p>{db.experience.present}</p>
       <p style={{ color: "#00000077", marginTop: "0" }}>
-        Sr. User experience designer, Foundations.
+        {db.experience.presentInfo}
       </p>
 
       <p>PROJECTS :</p>
       <p style={{ textAlign: "right", fontSize: "13px", color: "#00000088" }}>
-        Establishing design system artifacts and processes to improve design
-        across the rage of product design.
+        {db.projects.info}
       </p>
     </section>
   );

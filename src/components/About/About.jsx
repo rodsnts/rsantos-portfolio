87a26e8f7a10b180a2872c5b8e399e932b9c79fe
Rodrigo Santos <rr.santos1232@gmail.com>
2020-06-07 13:39:33 +0100

    education update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Header from "../Header/Header";
+import React from 'react';
+import Header from '../Header/Header';
 
-const db = require("../../db.json");
+const db = require('../../db.json');
 
 const About = () => {
   return (
@@ -10,20 +10,20 @@ const About = () => {
 
       <span
         className="mark"
-        style={{ width: "fit-content", marginTop: "20px" }}
+        style={{ width: 'fit-content', marginTop: '20px' }}
       >
-        <a href="https://pdfhost.io/v/6hOymXPBA_Front_End_Developer.pdf">
+        <a href="https://pdfhost.io/v/Ywv~29N6q_FrontEnd_Developer.pdf">
           YOU CAN SEE MY FULL RESUME HERE
         </a>
       </span>
 
-      <p style={{ marginTop: "45px" }}>Education :</p>
+      <p style={{ marginTop: '45px' }}>Education :</p>
 
       <p>{db.education.school}</p>
-      <p style={{ color: "#00000077", marginTop: "0" }}>{db.education.info}</p>
+      <p style={{ color: '#00000077', marginTop: '0' }}>{db.education.info}</p>
 
       <p>CONTRIBUTIONS :</p>
-      <p style={{ textAlign: "right", fontSize: "13px", color: "#00000088" }}>
+      <p style={{ textAlign: 'right', fontSize: '13px', color: '#00000088' }}>
         {db.projects.info}
       </p>
     </section>
